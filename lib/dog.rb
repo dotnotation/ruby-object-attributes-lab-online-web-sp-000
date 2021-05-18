9bbb0d8686247d79f44bf51b8138e0030d73ed82
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-18 17:38:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,7 @@
 class Dog
-  def name(dogs_name)
-    @name = dogs_name
-  end
-  def name
-    @name
-  end
-  def breed(breed_name)
-    @this_dogs_breed = breed_name
-  end
-  def breed
-    @this_dogs_breed
-  end
+ def name
+   @name
+ end
+ def name=(dogs_name)
+   @name = dogs_name
 end
